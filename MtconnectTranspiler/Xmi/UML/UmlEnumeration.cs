19e37e75b20f9bd57926caca7b36d3dcbb3c3d11
Trais McAllister <tbm0115@hotmail.com>
2026-01-06 23:40:01 -0500

    Added IsAbstract to UmlEnumeration

--- MtconnectTranspiler/Xmi/UML/UmlEnumeration.cs
+++ MtconnectTranspiler/Xmi/UML/UmlEnumeration.cs
@@ -20,6 +20,12 @@ namespace MtconnectTranspiler.Xmi.UML
         public UmlEnumerationLiteral[]? Items { get; set; }
 
         /// <summary>
+        /// <c>isAbstract</c> attribute
+        /// </summary>
+        [XmlAttribute(AttributeName = XmlHelper.XmiStructure.isAbstract, Namespace = "")]
+        public bool IsAbstract { get; set; }
+
+        /// <summary>
         /// Collection of <inheritdoc cref="UmlGeneralization"/>
         /// </summary>
         [XmlElement(ElementName = XmlHelper.XmiStructure.GENERALIZATION, Namespace = "")]
