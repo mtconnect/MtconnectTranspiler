27eee0dfcb45fb1ad8fe96257a8ec55e6961a0ef
Trais McAllister <tbm0115@hotmail.com>
2026-02-27 09:51:47 -0500

    Tbm0115/v2.7 (#27)
    * Updated to v2.7.0 MTConnect Standard
    
    * Update MtconnectTranspiler.csproj
    
    * Expanded XML coverage
    
    Coverage of all elements and attributes in the v2.7 release.

---
+++ MtconnectTranspiler/Xmi/MDCustomizationForSysMLAdditionalStereoTypes/ExternalModel.cs
@@ -0,0 +1,20 @@
+﻿using MtconnectTranspiler.Contracts;
+using System;
+using System.Xml.Serialization;
+
+namespace MtconnectTranspiler.Xmi.MDCustomizationForSysMLAdditionalStereoTypes
+{
+    /// <summary>
+    /// <c>&lt;MD_Customization_for_SysML__additional_stereotypes:ExternalModel /&gt;</c> element
+    /// </summary>
+    [Serializable, XmlRoot(ElementName = XmlHelper.MDCustomizationForSysMLAdditionalStereoTypes.EXTERNAL_MODEL, Namespace = XmlHelper.Md_Customization_for_SysML__additional_stereotypesNamespace)]
+    public class ExternalModel : ProfileElement
+    {
+        /// <summary>
+        /// <c>base_Element</c> attribute
+        /// </summary>
+        /// <remarks>Foreign key to the <see cref="XmiElement.Id"/> of the object this applies to.</remarks>
+        [XmlAttribute(AttributeName = XmlHelper.MDCustomizationForSysMLAdditionalStereoTypes.baseElement, Namespace = "")]
+        public string? BaseElement { get; set; }
+    }
+}
